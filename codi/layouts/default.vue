e3172f3becfebabeb03d738b9982160013a0a52d
mutrera <116823043+mutrera@users.noreply.github.com>
2022-11-21 15:27:48 +0100

    formulari

---
+++ codi/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <j-header></j-header>
+    <v-main>
+      <nuxt/>
+    </v-main>
+    <j-footer></j-footer>
+  </v-app>
+</template>
---
+++ codi/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-row>
+        <v-col>
+          <v-text-field 
+          label="Escriu el teu nom"
+          placeholder="p.e Sandra"
+          >
+          </v-text-field>
+          <v-text-field 
+          label="Escriu el teu nom"
+          solo>
+          </v-text-field>
+          <v-text-field 
+          label="Escriu el teu nom"
+          outlined>
+          </v-text-field>
+          <v-text-field 
+          label="Escriu el teu nom"
+          filled>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-row>
+  </v-container>
+</template>
+  
